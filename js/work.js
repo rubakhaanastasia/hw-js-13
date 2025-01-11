a4200afa0ceca081426be1e658c5d89f92b3cff2
qwerty <145857108+rubakhaanastasia@users.noreply.github.com>
2025-01-11 14:29:30 +0100

    Add files via upload

---
+++ js/work.js
@@ -0,0 +1,77 @@
+//1.
+// const user = {
+//   mood: "tired",
+//   hobby: "reading",
+//   premium: true,
+// };
+
+// const userMood = user.mood
+// const userHobby = user.hobby
+// const userPremium = user.premium
+// console.log(userMood, userHobby, userPremium);
+//--------------------------------------------------
+
+//2.
+const member = {
+  name: "anastasia",
+  age: 15,
+  hobby: "reading",
+};
+
+const memberName = member.name;
+const memberAge = member.age;
+const memberHobby = member.hobby;
+console.log(memberName, memberAge, memberHobby);
+console.log(memberName);
+console.log(memberAge);
+console.log(memberHobby);
+//--------------------------------------------------
+
+//3.
+const weather = {
+  temperature: -22,
+  interest: 80,
+  courseWind: 10,
+};
+
+const weatherTemperature = weather.temperature;
+const weatherInterest = weather.interest;
+const weatherCourseWind = weather.courseWind;
+console.log(weatherTemperature);
+console.log(weatherInterest);
+console.log(weatherCourseWind);
+//--------------------------------------------------
+
+//4.
+const user = {
+  name: "anastasia rubakha",
+  email: "kjtmetntiksd@gmail.com",
+  password: "kfdskfjsl",
+};
+
+const userName = user.name;
+const userEmail = user.email;
+const userPassword = user.password;
+console.log(userName, userEmail, userPassword);
+//--------------------------------------------------
+
+//5.
+const movie = {
+  title: "your heart will be broken",
+  director: "Anna Jane",
+  year: 2020,
+  rating: 9.1,
+};
+
+const movieTitle = movie.title;
+const movieDirector = movie.director;
+const movieYear = movie.year;
+const movieRating = movie.rating;
+console.log(movieTitle, movieDirector, movieYear, movieRating);
+//--------------------------------------------------
+
+//6.
+const account = {
+    balance: 1000,
+    
+   }
